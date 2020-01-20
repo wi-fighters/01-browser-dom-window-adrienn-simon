591f81f2da068b86c5ecb8adcb8462060d93e563
Nicolas <nicolas@codingmonk.dev>
2020-01-20 15:18:40 +0100

    added while loop to script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 (() => {
     const promptForGuesses = number => {
-        // initialize attempts to 0
+
         const attempts = 0;
-        // initialize the prompt message "Guess a number between 1 and 10"
+
         let userNumber = prompt(`Guess a number between 1 and 10:`);
+
 
         while (attempts < 3) {
 
-        }
+            if (randomNumber !== parseInt(userNumber, 10)) {
+                prompt(`"Nope, sorry, try again"`);
+                attempts++;
+            } else {
+                prompt(`You reached max Numbers of tries`);
+            } if (randomNumber === parseInt(userNumber, 10)) {
+                prompt(`Yeah! Congratulations you made it!`);
+            }
 
+        };
 
-        // while (attempts < 3) {
-        // prompt the user with the prompt message
-        // increment the number of attempts
 
         // if (the user guesses a correct number)
         // print success message and number of attempts
@@ -26,7 +32,7 @@
         // print fail message: "Sorry, you failed to guess the number in three attempts. The number was 2!"
     };
 
-    const randomNumber = Math.floor(Math.random() * 10) + 1;
 
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
     promptForGuesses(randomNumber);
 })();
