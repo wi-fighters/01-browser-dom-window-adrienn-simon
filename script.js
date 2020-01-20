17ad5a7763eb587091641895999d828d2ce23e40
Nicolas <nicolas@codingmonk.dev>
2020-01-20 14:06:51 +0100

    updated the sudo code

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// generate a random number
+(() => {
+    const promptForGuesses = number => {
+        // initialize attempts to 0 // 3
+        // initialize the prompt message "Guess a number between 1 and 10"
 
-// prompt user for a number
+        // while (attempts < 3) {
+        // prompt the user with the prompt message
+        // increment the number of attempts
 
-// Compare Numbers
+        // if (the user guesses a correct number)
+        // print success message and number of attempts
+        // stop the loop (TODO: break? return?)
 
-// if correct - prompt correct!
+        // else 
+        // update prompt message: "Nope, sorry, try again"
+        // }
 
-// if wrong - prompt "You have n-attempts"
+        // assuming we didn't return before
+        // print fail message: "Sorry, you failed to guess the number in three attempts. The number was 2!"
+    };
 
-// 
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+    promptForGuesses(randomNumber);
+})();
