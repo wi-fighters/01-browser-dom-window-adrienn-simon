770a9d4c35f56158831f230edfebb249d7c9f161
Adrienn <adrienntollas@gmail.com>
2020-01-20 11:53:39 +0100

    create win-alert

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const randomNumber = Math.ceil(Math.random() * 10);
+const randomizer = Math.ceil(Math.random() * 10);
+let randomNumber = randomizer;
+let playerNumber = prompt("Guess a number between 1-10:");
+let attempts = 0;
+
+if (playerNumber === randomNumber) {
+    alert(`Success, the number was ${
+        randomNumber
+    }! Attempts: ${attempts}`)
+}
