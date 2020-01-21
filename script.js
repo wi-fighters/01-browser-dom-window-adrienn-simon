6415153f67d68405372e5ea793c217f7af5b5bed
Nicolas <nicolas@codingmonk.dev>
2020-01-21 11:35:26 +0100

    finished the script.js file

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
 (() => {
     const promptForGuesses = number => {
 
-        const attempts = 0;
+        let attempts = 0;
 
         let userNumber = prompt(`Guess a number between 1 and 10:`);
 
 
-        while (attempts < 3) {
+        while (attempts < 2) {
 
             if (randomNumber !== parseInt(userNumber, 10)) {
-                prompt(`"Nope, sorry, try again"`);
-                attempts++;
-            } else {
-                prompt(`You reached max Numbers of tries`);
+                prompt(`Nope, sorry, try again. You have still ${2 - attempts} attempts left.`);
+                attempts += 1;
             } if (randomNumber === parseInt(userNumber, 10)) {
                 prompt(`Yeah! Congratulations you made it!`);
+                return randomNumber;
             }
-
+            if (attempts === 2) {
+                alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber} !`);
+            }
         };
 
-
-        // if (the user guesses a correct number)
-        // print success message and number of attempts
-        // stop the loop (TODO: break? return?)
-
-        // else 
-        // update prompt message: "Nope, sorry, try again"
-        // }
-
-        // assuming we didn't return before
-        // print fail message: "Sorry, you failed to guess the number in three attempts. The number was 2!"
     };
 
 
