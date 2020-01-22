4551571d1e8498dd0bb9f4b1bc509b0bfd9a2249
Nicolas <nicolas@codingmonk.dev>
2020-01-22 10:20:10 +0100

    added a return at line 18 in script.js

--- script.js
+++ script.js
@@ -2,21 +2,20 @@
     const promptForGuesses = number => {
 
         let attempts = 0;
-
         let userNumber = prompt(`Guess a number between 1 and 10:`);
-
 
         while (attempts < 2) {
 
             if (randomNumber !== parseInt(userNumber, 10)) {
                 prompt(`Nope, sorry, try again. You have still ${2 - attempts} attempts left.`);
                 attempts += 1;
-            } if (randomNumber === parseInt(userNumber, 10)) {
+            }
+            if (randomNumber === parseInt(userNumber, 10)) {
                 prompt(`Yeah! Congratulations you made it!`);
                 return randomNumber;
             }
             if (attempts === 2) {
-                alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber} !`);
+                return alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber} !`);
             }
         };
 
