526eaefa049190bfc4a36502a5f95cacee67cfb5
Nicolas <nicolas@codingmonk.dev>
2020-01-22 11:45:41 +0100

    changed variable name userNumber to userGuess

--- script.js
+++ script.js
@@ -2,26 +2,30 @@
     const promptForGuesses = number => {
 
         let attempts = 0;
-        let userNumber = prompt(`Guess a number between 1 and 10:`);
+        let userGuess = prompt(`Guess a number between 1 and 10:`);
 
-        while (attempts < 2) {
 
-            if (randomNumber !== parseInt(userNumber, 10)) {
+        while (attempts < 3) {
+
+            if (randomNumber !== parseInt(userGuess, 10)) {
                 prompt(`Nope, sorry, try again. You have still ${2 - attempts} attempts left.`);
                 attempts += 1;
             }
-            if (randomNumber === parseInt(userNumber, 10)) {
+            if (randomNumber === parseInt(userGuess, 10)) {
                 prompt(`Yeah! Congratulations you made it!`);
                 return randomNumber;
             }
             if (attempts === 2) {
-                return alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber} !`);
+                alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber} !`);
+                return;
             }
         };
+
 
     };
 
 
     const randomNumber = Math.floor(Math.random() * 10) + 1;
     promptForGuesses(randomNumber);
+    // console.log(promptForGuesses(randomNumber));
 })();
